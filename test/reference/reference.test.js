4807c5d0eca0c7f9db36f63c5d4f28b9f7e9fa33
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-01-23 17:21:58 +0000

    Updating references tests to include the five additional Worker reference sets (Ethnicity, Nationality, Country of Birth, Qualifications and RecruitedFrom). Also updated the LaS test snapshots (the reference data had changed).

--- test/reference/reference.test.js
+++ test/reference/reference.test.js
@@ -53,4 +53,38 @@ describe ("Expected reference services", async () => {
         expect(laS.body).toMatchSnapshot();
     });
 
+    it("should fetch Ethnicities", async () => {
+        const ethnicities = await apiEndpoint.get('/ethnicity')
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(ethnicities.body).toMatchSnapshot();
+    });
+
+    it("should fetch Nationalities", async () => {
+        const nationalities = await apiEndpoint.get('/nationality')
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(nationalities.body).toMatchSnapshot();
+    });
+
+    it("should fetch countries", async () => {
+        const countries = await apiEndpoint.get('/country')
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(countries.body).toMatchSnapshot();
+    });
+    
+    it("should fetch qualifications", async () => {
+        const qualification = await apiEndpoint.get('/qualification')
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(qualification.body).toMatchSnapshot();
+    });
+    
+    it("should fetch qualifications", async () => {
+        const recruitedFrom = await apiEndpoint.get('/recruitedFrom')
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(recruitedFrom.body).toMatchSnapshot();
+    });
 });
