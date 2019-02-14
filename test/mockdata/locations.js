df075d4f0e77456228e6f41b1766085981dadc06
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-02-14 16:49:04 +0000

    Revised locations set following recent refresh which cut by half the number of valid locations

--- test/mockdata/locations.js
+++ test/mockdata/locations.js
@@ -1,58 +1,53 @@
 // http://localhost:3000/api/test/locations/random?limit=5
 exports.data = [
     {
-        "cqcid": 10328,
-        "address1": "3 Warwick House",
-        "address2": "Ratcliffe Street",
-        "townAndCity": "Atherstone",
-        "county": "Warwickshire",
-        "postcode": "CV9 1JP",
-        "mainServiceName": "Homecare agencies",
-        "locatioName": "Voyage (DCA) Warwickshire",
-        "locationId": "1-1212502498"
-    },
-    {
-        "cqcid": 54403,
-        "address1": "26-27 Bedford Square",
-        "address2": null,
-        "townAndCity": "London",
-        "county": null,
-        "postcode": "WC1B 3HP",
-        "mainServiceName": "Diagnosis/screening",
-        "locatioName": "Acculabs Diagnostics - London",
-        "locationId": "1-302105140"
-    },
-    {
-        "cqcid": 3523,
-        "address1": "Parkside Hospital",
-        "address2": "53 Parkside",
-        "townAndCity": "Wimbledon",
-        "county": "Surrey",
-        "postcode": "SW19 5NX",
-        "mainServiceName": "Doctors/Gps",
-        "locatioName": "Nippon Club South Clinic",
-        "locationId": "1-113123848"
-    },
-    {
-        "cqcid": 78912,
-        "address1": "Wakefield Road",
-        "address2": null,
-        "townAndCity": "Cockermouth",
-        "county": "Cumbria",
-        "postcode": "CA13 0HZ",
-        "mainServiceName": "Doctors/Gps",
-        "locatioName": "Derwent House Surgery",
-        "locationId": "1-583652477"
-    },
-    {
-        "cqcid": 45233,
-        "address1": "Sutton Road",
-        "address2": null,
-        "townAndCity": "Maidstone",
+        "address1": "69 The Brent",
+        "address2": "Dartford",
+        "townAndCity": "Kent",
         "county": "Kent",
-        "postcode": "ME15 8RA",
+        "postcode": "DA1 1YD",
+        "mainServiceName": "Dentist",
+        "locatioName": "Smiledartford",
+        "locationId": "1-226594347"
+    },
+    {
+        "address1": "Lancaster Road",
+        "address2": "Forton",
+        "townAndCity": "Preston",
+        "county": "Lancashire",
+        "postcode": "PR3 0BL",
         "mainServiceName": "Nursing homes",
-        "locatioName": "Ashley Gardens Care Centre",
-        "locationId": "1-2247278847"
+        "locatioName": "Oakfield Nursing Home",
+        "locationId": "1-144413147"
+    },
+    {
+        "address1": "1st Floor, Boots the Chemist",
+        "address2": "Dolphin Shopping Centre",
+        "townAndCity": "Poole",
+        "county": "Dorset",
+        "postcode": "BH15 1SX",
+        "mainServiceName": "Blood and transplant service",
+        "locatioName": "Poole Donor Centre",
+        "locationId": "T1460W8"
+    },
+    {
+        "address1": "2A Trevarthian Road",
+        "address2": null,
+        "townAndCity": "St Austell",
+        "county": "Cornwall",
+        "postcode": "PL25 4BH",
+        "mainServiceName": "Homecare agencies",
+        "locatioName": "Victoria Community Care (Cornwall) Limited",
+        "locationId": "1-482056745"
+    },
+    {
+        "address1": "Mansfield Community Hospital",
+        "address2": "Stockwell Gate",
+        "townAndCity": "Mansfield",
+        "county": "Nottinghamshire",
+        "postcode": "NG18 5QJ",
+        "mainServiceName": "Dentist",
+        "locatioName": "Mansfield Clinic",
+        "locationId": "RHAY9"
     }
 ];
